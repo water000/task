6032bc0a8c4c23d2d4e854bb3ca48cbb2548220f
tiger <wanghu8242837@163.com>
2015-12-04 08:03:57 +0800

    modified:   ModuleBase/_wwwroot/static/js/global.js 	modified:   ModuleBase/info/action/edit.php 	modified:   ModuleBase/info/config/default.php 	modified:   ModuleBase/merchant/action/edit.php 	modified:   ModuleBase/merchant/class/CMctAttachmentControl.php 	modified:   ModuleBase/merchant/moddef/CMerchantDef.php

--- ModuleBase/_wwwroot/static/js/global.js
+++ ModuleBase/_wwwroot/static/js/global.js
@@ -148,10 +148,10 @@ function fileUpload(opt){
 	var _edit = function(inputFile){
 		var _win = document.createElement("span");
 		_win.id = "img-lab-bg";
-		_win.appendChild(inputFile);
-		//inputFile.style.display = "none";
 		cntr.insertBefore(_win, cntr.childNodes[0]);
-		_win.innerHTML += "<label title='删除' class='img-lab-del' id='img-lab'>-</label><div class=img-name></div>";
+		_win.innerHTML = "<label title='删除' class='img-lab-del' id='img-lab'>-</label><div class=img-name></div>";
+		inputFile.style.display = "none";
+		_win.insertBefore(inputFile, _win.childNodes[0]);
 		for(var c,n=inputFile.value.length-1; n>=0; n--){
 			c = inputFile.value.charAt(n);
 			if(c == '/' || c == '\\') break;
--- ModuleBase/info/config/default.php
+++ ModuleBase/info/config/default.php
@@ -1,12 +1,8 @@
 <?php 
 
 $default = array(
-<<<<<<< HEAD
-	'web_socket' => array('121.42.149.159', '8080', '/PushServer/chat'),
-=======
 	'web_socket'         => array('121.42.149.159', '8080', '/PushServer/chat'),
 	'download_file_host' => $_SERVER['HTTP_HOST'],
->>>>>>> 34fb3f7efb340cde68392838046ce78e5cca682d
 );
 
 
