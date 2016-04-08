736c6a51ab8f9935e71450ed78291eba3691b94c
wang hu <wanghu8242837@163.com>
2016-04-08 16:21:27 +0800

    modified:   ModuleBase/task/moddef/CTaskDef.php

--- ModuleBase/task/moddef/CTaskDef.php
+++ ModuleBase/task/moddef/CTaskDef.php
@@ -89,136 +89,50 @@ class CTaskDef extends CModDef {
                     self::G_DC  => '',
                     self::P_ARGS => array(
                         'category_id' => array(self::PA_REQ=>1, self::G_DC=>'分类id', self::PA_TYP=>'integer'),
+                        'res_page_id' => array(self::PA_REQ=>0, self::G_DC=>'分页id', self::PA_TYP=>'integer'),
                     ),
-                    self::P_OUT => '{has_more:0/1, list:[{id:1, 详见task_info表}]}',
+                    self::P_OUT => '{has_more:0/1, list:[{id:1, 详见task_info表}, ...]}',
+                ), 
+                'category_list' => array(
+                    self::P_TLE => '分类列表',
+                    self::G_DC  => '',
+                    self::P_ARGS => array(),
+                    self::P_OUT => '{{id:1, name:"", icon:""}, ...}'
                 ),
-                'logout' => array(
+                'detail' => array(
+                    self::P_TLE => '任务详情',
+                    self::G_DC  => '',
+                    self::P_ARGS => array(
+                        'task_id' => array(self::PA_REQ=>1, self::G_DC=>'任务id', self::PA_TYP=>'integer'),
+                    ),
+                    self::P_OUT => '{id:1, 详见task_info表}'
+                ),
+                'submit_stat' => array(
+                    self::P_TLE => '提交汇总',
+                    self::G_DC  => '',
+                    self::P_ARGS => array(
+                        'task_id' => array(self::PA_REQ=>1, self::G_DC=>'任务id', self::PA_TYP=>'integer'),
+                    ),
+                    self::P_OUT => '{total:10, used:3: unused:6: unread:1}'
+                ),
+                'submit_list' => array(
+                    self::P_TLE => '提交列表',
+                    self::G_DC  => '',
+                    self::P_ARGS => array(
+                        'task_id' => array(self::PA_REQ=>1, self::G_DC=>'任务id', self::PA_TYP=>'integer'),
+                    ),
+                    self::P_OUT => '{has_more:0/1, list:[{id:1, submit_uid, submit_uname, 详见task_submit表}, ...]}'
+                ),
+                /*'logout' => array(
                     self::P_TLE => '注销',
                     self::G_DC  => '注销当前已登录的用户',
                     self::P_OUT => '{retcode:"SUCCESS"}'
-                ),
-                'edit' => array(
-                    self::P_TLE => '编辑',
-                    self::G_DC  => '编辑用户信息',
-                    self::P_MGR => true,
-                    self::P_NCD => true,
-                    self::P_ARGS => array(
-                        'name'         => array(self::PA_REQ=>1, self::PA_EMP=>0, self::G_DC=>'名称', self::PA_RNG=>'2, 17'),
-                        'password'     => array(self::PA_REQ=>1, self::PA_EMP=>0, self::G_DC=>'密码', self::PA_RNG=>'6, 17'),
-                        'organization' => array(self::PA_REQ=>0, self::G_DC=>'单位', self::PA_RNG=>'2, 32'),
-                        'phone'        => array(self::PA_REQ=>1, self::PA_EMP=>0, self::G_DC=>'手机', self::PA_RNG=>'11, 12'),
-                        'email'        => array(self::PA_REQ=>0, self::G_DC=>'邮箱', self::PA_RNG=>'6, 255'),
-                        'IMEI'         => array(self::PA_REQ=>0, self::G_DC=>'IMEI', self::PA_RNG=>'6, 32'),
-                        'IMSI'         => array(self::PA_REQ=>0, self::G_DC=>'IMSI', self::PA_RNG=>'6, 32'),
-                        'class_id'     => array(self::PA_REQ=>0, self::G_DC=>'分类id'),
-                        'VPDN_name'    => array(self::PA_REQ=>0, self::G_DC=>'VPDN名称', self::PA_RNG=>'6, 32'),
-                        'VPDN_pass'    => array(self::PA_REQ=>0, self::G_DC=>'VPDN密码', self::PA_RNG=>'6, 32'),
-                        'class_id'     => array(self::PA_REQ=>0, self::G_DC=>'分类id', self::PA_TYP=>'integer', self::PA_RNG=>'1, 4'),
-                    ),
-                ),
-                'list' => array(
-                    self::P_TLE => '用户管理',
-                    self::G_DC  => '用户的列表，也可以搜索用户(phone, name, email)，都是精确查询，不支持模糊查询',
-                    self::P_MGR => true
-                ),
-                'class' => array(
-                    self::P_TLE => '分类管理',
-                    self::G_DC  => '获取、删除用户分类',
-                    self::P_MGR => true,
-                    	
-                ),
-                'class_edit' => array(
-                    self::P_TLE => '分类编辑',
-                    self::G_DC => '对分类进行批量编辑',
-                    self::P_MGR => true,
-                    self::P_NCD => true,
-                    self::P_ARGS => array(
-                        'name'         => array(self::PA_REQ=>1, self::PA_EMP=>0, self::G_DC=>'名称', self::PA_RNG=>'2, 16'),
-                        'code'         => array(self::PA_REQ=>1, self::PA_EMP=>0, self::G_DC=>'编码', self::PA_RNG=>'2, 32'),
-                    ),
-                ),
-                'department' => array(
-                    self::P_TLE => '部门管理',
-                    self::G_DC => '添加、删除部门，及获取列表',
-                    self::P_MGR => true,
-                    	
-                ),
-                'dep_edit' => array(
-                    self::P_TLE => '部门编辑',
-                    self::G_DC => '对部门进行批量编辑, 以及加入指定用户到当前部门',
-                    self::P_MGR => true,
-                    self::P_NCD => true,
-                    self::P_ARGS => array(
-                        'name'     => array(self::PA_REQ=>1, self::PA_EMP=>0, self::G_DC=>'名称', self::PA_RNG=>'2,16'),
-                        'password' => array(self::PA_REQ=>1, self::PA_EMP=>0, self::G_DC=>'密码', self::PA_RNG=>'6,'),
-                    )
-                ),
-                'myinfo'  => array(
-                    self::P_TLE => '我的信息',
-                    self::G_DC  => '显示或修改我的信息，如果提供参数列表中的参数时，则进行修改.没有则返回当前用户信息',
-                    self::P_ARGS => array(
-                        'pwd1'     => array(self::PA_REQ=>1, self::G_DC=>'新密码', self::PA_RNG=>'6, 32'),
-                        'pwd2'     => array(self::PA_REQ=>1, self::G_DC=>'确认密码，须与1密码相同', self::PA_RNG=>'6, 32'),
-                        'src_pwd'  => array(self::PA_REQ=>1, self::G_DC=>'原来密码', self::PA_RNG=>'6, 32'),
-                    ),
-                    self::P_OUT => '{retcode:"SUCCESS/ERROR_MSG", data:{user-info}',
-                    self::LD_FTR => array(
-                        array('user', 'checkLogin', true)
-                    ),
-                )
+                ),*/
+                
             ),
         );
     }
 
-    const BANNED_DEL_MAX_CLASS_ID = 3;
-
-    function install($dbpool, $mempool=null){
-        parent::install($dbpool, $mempool);
-
-        mbs_import('', 'CUserControl', 'CUserClassControl', 'CUserDepControl');
-        try {
-            $ins = CUserControl::getInstance(self::$appenv, $dbpool, $mempool);
-            $uid = $ins->add(array(
-                'id'        => 1,
-                'name'      => 'admin',
-                'password'  => CUserControl::formatPassword('123321'),
-                'phone'     => '13666666666',
-                'reg_time'  => time(),
-                'reg_ip'    => self::$appenv->item('client_ip')
-            ));
-            $uid = $ins->add(array(
-                'id'        => 2,
-                'name'      => 'developer',
-                'password'  => CUserControl::formatPassword('123123'),
-                'phone'     => '13888888888',
-                'reg_time'  => time(),
-                'reg_ip'    => self::$appenv->item('client_ip')
-            ));
-            	
-            //由于系统的要求，分类和部门的id需要一一对应.且在CUserDepControl中的$DEP_MAP的内容也是遵循这里的顺序
-            $uc = CUserClassControl::getInstance(self::$appenv, $dbpool, $mempool);
-            $pre_class = array(
-                array('id' => 1, 'name' => '干警', 'code' => 'POLICE', 'create_time' => time()),
-                array('id' => 2, 'name' => '厅、处领导', 'code' => 'TC_LDR', 'create_time' => time()),
-                array('id' => 3, 'name' => '省委领导', 'code' => 'PV_LDR', 'create_time' => time()),
-            );
-            foreach($pre_class as $c){
-                $uc->add($c);
-            }
-            	
-            $ud = CUserDepControl::getInstance(self::$appenv, $dbpool, $mempool);
-            $pre_dep = array(
-                array('id' => 1, 'name' => '业务系统查询结果', 'password'=>'123123', 'edit_time'=>time()),
-                array('id' => 2, 'name' => '舆情报告', 'password'=>'123123', 'edit_time'=>time()),
-                array('id' => 3, 'name' => '声像信息（视频）', 'password'=>'123123', 'edit_time'=>time()),
-            );
-            foreach($pre_dep as $d){
-                $ud->add($d);
-            }
-        } catch (Exception $e) {
-            throw $e;
-        }
-    }
 }
 
 ?>
